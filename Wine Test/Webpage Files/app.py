ea4ec233c5a2b50c9d522189986c4e0893a5499b
Madeline Starr <40212869+ariadne27@users.noreply.github.com>
2019-09-18 14:40:08 -0400

    update with data

--- Wine Test/Webpage Files/app.py
+++ Wine Test/Webpage Files/app.py
@@ -27,8 +27,8 @@ Base = automap_base()
 Base.prepare(db.engine, reflect=True)
 
 # Save references to each table
-Samples_Metadata = Base.classes.samples_metadata
-Samples = Base.classes.samples
+Samples_Metadata = Base.classes.attributes
+# Samples = Base.classes.ID
 
 
 @app.route("/")
@@ -42,63 +42,65 @@ def names():
     """Return a list of sample names."""
 
     # Use Pandas to perform the sql query
-    stmt = db.session.query(Samples).statement
+    stmt = db.session.query(Samples_Metadata).statement
     df = pd.read_sql_query(stmt, db.session.bind)
 
     # Return a list of the column names (sample names)
-    return jsonify(list(df.columns)[2:])
+    return jsonify(list(df.ID)[2:])
 
 
-@app.route("/metadata/<sample>")
-def sample_metadata(sample):
+@app.route("/metadata/<ID>")
+def sample_metadata(ID):
     """Return the MetaData for a given sample."""
     sel = [
-        Samples_Metadata.sample,
-        Samples_Metadata.ETHNICITY,
-        Samples_Metadata.GENDER,
-        Samples_Metadata.AGE,
-        Samples_Metadata.LOCATION,
-        Samples_Metadata.BBTYPE,
-        Samples_Metadata.WFREQ,
+        Samples_Metadata.ID,
+        Samples_Metadata.tier_1,
+        Samples_Metadata.tier_2,
+        Samples_Metadata.attribute,
+        Samples_Metadata.wine1,
+        Samples_Metadata.wine2,
+        Samples_Metadata.wine3,
+        Samples_Metadata.wine4,
+        Samples_Metadata.wine5
     ]
 
-    results = db.session.query(*sel).filter(Samples_Metadata.sample == sample).all()
+    results = db.session.query(*sel).filter(Samples_Metadata.ID == ID).all()
 
     # Create a dictionary entry for each row of metadata information
     sample_metadata = {}
     for result in results:
-        sample_metadata["sample"] = result[0]
-        sample_metadata["ETHNICITY"] = result[1]
-        sample_metadata["GENDER"] = result[2]
-        sample_metadata["AGE"] = result[3]
-        sample_metadata["LOCATION"] = result[4]
-        sample_metadata["BBTYPE"] = result[5]
-        sample_metadata["WFREQ"] = result[6]
+        sample_metadata["ID"] = result[0]
+        sample_metadata["Attribute"] = result[3]
+        sample_metadata["Wine 1:"] = result[4]
+        sample_metadata["Wine 2"] = result[5]
+        sample_metadata["Wine 3"] = result[6]
+        sample_metadata["Wine 4"] = result[7]
+        sample_metadata["Wine 5"] = result[8]
 
     print(sample_metadata)
     return jsonify(sample_metadata)
 
 
-@app.route("/samples/<sample>")
-def samples(sample):
-    """Return `otu_ids`, `otu_labels`,and `sample_values`."""
-    stmt = db.session.query(Samples).statement
-    df = pd.read_sql_query(stmt, db.session.bind)
+# @app.route("/samples/<sample>")
+# def samples(sample):
+#     """Return `otu_ids`, `otu_labels`,and `sample_values`."""
+#     stmt = db.session.query(Samples).statement
+#     df = pd.read_sql_query(stmt, db.session.bind)
 
-    # Filter the data based on the sample number and
-    # only keep rows with values above 1
-    sample_data = df.loc[df[sample] > 1, ["otu_id", "otu_label", sample]]
+#     # Filter the data based on the sample number and
+#     # only keep rows with values above 1
+#     sample_data = df.loc[df[sample] > 1, ["otu_id", "otu_label", sample]]
 
-    # Sort by sample
-    sample_data.sort_values(by=sample, ascending=False, inplace=True)
+#     # Sort by sample
+#     sample_data.sort_values(by=sample, ascending=False, inplace=True)
 
-    # Format the data to send as json
-    data = {
-        "otu_ids": sample_data.otu_id.values.tolist(),
-        "sample_values": sample_data[sample].values.tolist(),
-        "otu_labels": sample_data.otu_label.tolist(),
-    }
-    return jsonify(data)
+#     # Format the data to send as json
+#     data = {
+#         "otu_ids": sample_data.otu_id.values.tolist(),
+#         "sample_values": sample_data[sample].values.tolist(),
+#         "otu_labels": sample_data.otu_label.tolist(),
+#     }
+#     return jsonify(data)
 
 
 if __name__ == "__main__":
--- Wine Test/Webpage Files/static/js/app.js
+++ Wine Test/Webpage Files/static/js/app.js
@@ -152,3 +152,29 @@ function buildMetadata(sample) {
       });
    });
   }
+function init() {
+// Grab a reference to the dropdown select element
+    var selector = d3.select("#selDataset");
+
+    // Use the list of sample names to populate the select options
+    d3.json("/names").then((sampleNames) => {
+        sampleNames.forEach((sample) => {
+        selector
+            .append("option")
+            .text(sample)
+            .property("value", sample);
+        });
+
+        // Use the first sample from the list to build the initial plots
+        const firstSample = sampleNames[1];
+        buildMetadata(firstSample);
+    });
+}
+
+function optionChanged(newSample) {
+// Fetch new data each time a new sample is selected
+buildMetadata(newSample);
+}
+
+// Initialize the dashboard
+init();
